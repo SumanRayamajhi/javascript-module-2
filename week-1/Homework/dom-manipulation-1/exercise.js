6c80e725fd0933580030317e593b192deb2d9e6a
Suman Rayamajhi <meh_sams@yahoo.com>
2021-08-02 12:24:01 +0200

    completed homework

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -11,20 +11,20 @@ Write JavaScript below that logs:
     2. the first div element node
     --> should log the ".site-header" node
     */
-    const siteHeader = document.querySelector(".site-header");
-    console.log(siteHeader);
-    /*
-    3. the element with id "jumbotron-text"
-    --> should log the "#jumbotron-text" node
-    */
-    const jumbotronText = document.querySelector("#jumbotron-text");
-    console.log(jumbotronText);
-    /*
-    4. all the "p" elements of contained inside  the .primary-content element node
-    --> should log a list of nodes with a length of 3
-    */
-    const pPrimaryContent = document.querySelector(".primary-content p");
-    console.log(pPrimaryContent);
+     const siteHeader = document.querySelector("div");
+     console.log(siteHeader);
+     /*
+     3. the element with id "jumbotron-text"
+     --> should log the "#jumbotron-text" node
+     */
+     const jumbotronText = document.querySelector("#jumbotron-text");
+     console.log(jumbotronText);
+     /*
+     4. all the "p" elements of contained inside  the .primary-content element node
+     --> should log a list of nodes with a length of 3
+     */
+     const pPrimaryContent = document.querySelectorAll(".primary-content p");
+     console.log(pPrimaryContent);
 
 
 /*
@@ -47,7 +47,8 @@ Write JavaScript below that changes the background colour of the page when the '
 */
 const changeBgColor = document.querySelector("#bgrChangeBtn");
 changeBgColor.addEventListener("click", () => {
-    document.body.style.backgroundColor = " #b3b3ff";
+    // document.body.style.backgroundColor = "#b3b3ff";
+    document.body.classList.toggle("site-filter");
 })
 
 /*
@@ -57,16 +58,17 @@ Task 4
 When a user clicks the ‘Add some text’ button, a new paragraph should be added inside the section that says “LEARN MORE”
 */
 
-const addSomeText = document.querySelector("#addTextBtn");
+let addSomeText = document.querySelector("#addTextBtn");
 
-addSomeText.addEventListener("click", () => {
-   const newP = document.createElement("p");
-   const text = document.createTextNode("Practicing to add more paragraph somewhere.");
-   newP.appendChild(text);
+addSomeText.addEventListener("click", addNewParagraph);
+function addNewParagraph() {
+   let newP = document.createElement("p");
+   let text = document.createTextNode("Practicing to add more paragraph somewhere.");
+   newP.prepend(text);
 
-   const learnMoreSection = document.querySelector(".articles");
-   learnMoreSection.appendChild(newP)
-})
+   let learnMoreSection = document.querySelector("#someText");
+   learnMoreSection.prepend(newP)
+}
 
 /*
 Task 5
@@ -74,13 +76,17 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
 const largeLinks = document.querySelector("#largerLinksBtn");
 
 largeLinks.addEventListener("click", () => {
-    const aEl = document.getElementsByTagName("a");
-    for (let i = 0; i < aEl.length; i++) {
-        aEl[i].style.fontSize = "1.1rem"
-    }
+    const aEl = document.querySelectorAll("a");
+    aEl.forEach((links) => {
+        links.style.fontSize = "1.8em";
+    });
+    // for (let i = 0; i < aEl.length; i++) {
+    //     aEl[i].style.fontSize = "1.1rem"
+    // }
     })
 
 
@@ -94,28 +100,19 @@ Also clear the text inside the input field
 */
 
 
-//    const learnMoreSection = document.querySelector(".articles");
-//    learnMoreSection.appendChild(newP)
-// }
-// var targetDiv = document.getElementsById("addArticleBtn").addEventListener("click", noParagraph);
-// function newParagraph(){
-//     var newTag = document.createElement("p");
-//     var newText = document.createTextNode("This is a new paragraph");
-//     console.log(newTag);
-//     var inputAtFirst = document.getElementsById("mainArticles");
-//     let result = inputAtFirst.insertBefore(newTag, inputAtFirst.childNodes[1]);
-//     result.style.color = "red";
+let addText = document.querySelector("#addArticleBtn");
 
-// }
-const addBtn = document.getElementById("addArticleBtn");
-addBtn.addEventListener('click', () => {
-    const inputText = document.getElementsByClassName("addArticle");
-    const newP = document.createElement("p");
-    newP.textContent = inputText.value;
-    const learnSection = document.querySelector("#mainArticles");
-    learnSection.appendChild(newP)
-    inputText.value = ""    
+addText.addEventListener("click", () => {
+//    addNewParagraph();
+   let newPp = document.createElement("p");
+   let textEl = document.querySelector(".form-control").value;
+   newPp.textContent = textEl;
+   let learnMore = document.querySelector("#mainArticles");
+   learnMore.prepend(newPp);
+   document.querySelector(".form-control").value = "";
+
 })
+
 /*
 Task 7
 ======
@@ -126,16 +123,29 @@ The next color when you are in the last color of the array will be the first col
 */
 
 
-const colors = ["#b3ffff", "#b3b3ff", " #ff99c2", "#ddff99", "#ffff99"]; 
+const colors = ["red", "#b3b3ff", " #ff99c2", "#ddff99", "#ffff99"]; 
 const backGroundColor = document.querySelector("body");
 let i = 0;
-changeBgColor.addEventListener('click', () => {
+changeBgColor.addEventListener("click", () => {
+    backGroundColor.style.backgroundColor = colors[i]
     if(i < colors.length - 1) {
         i++;
     } else {
         i = 0;
     }
-backGroundColor.style.backgroundColor = colors[i]
 })
+// const colors = ["site-filter-red", "site-filter-green", "site-filter-yellow", "site-filter-blue", "site-filter-blueviolet"]; 
+// const bodyElement = document.querySelector("body");
+// let i = 0;
+// changeBgColor.addEventListener("click", () => {
+//     bodyElement.classList.toggle(colors[i]);
+    
+//     if(i < colors.length - 1) {
+//         i++;
+//     } else {
+//         i = 0;
+//     }
+   
+// })
 
 
--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -27,15 +27,108 @@ greenBtn.addEventListener("click", () => {
     volunteerBtn.style.backgroundColor = "#8c9c08";
 })
 
-const emailArea = document.querySelector("#exampleInputEmail1");
-const nameArea = document.querySelector("#example-text-input");
-const testArea = document.querySelector("#xampleTextarea");
-const formControl = document.querySelector(".form-control");
+let submitButton = document.querySelector("form button");
 
-const submit = document.querySelector("form .btn-primary");
+    submitButton.addEventListener("click", (event) => {
+        event.preventDefault();
 
-submit.addEventListener("click", () => {
-    if(formControl.length < 0) {
-        return formControl.style.backgroundColor = "red";
-    }
-})
+        let emailArea = document.querySelector("#exampleInputEmail1");
+        let nameArea = document.querySelector("#example-text-input");
+        let textArea = document.querySelector("#exampleTextarea");
+    
+        let isEmailAreaValid = emailArea.value.trim().includes("@");
+        let isNameAreaValid = nameArea.value.trim().length > 0;
+        let isTextAreaValid = textArea.value.trim().length > 0;
+    
+        if(isEmailAreaValid && isNameAreaValid && isTextAreaValid) {
+            emailArea.style.backgroundColor = "transparent";
+            nameArea.style.backgroundColor = "transparent";
+            textArea.style.backgroundColor = "transparent";
+            emailArea.value = "";  
+            nameArea.value = ""; 
+            textArea.value = "";
+            alert("Thank you for filling out the form");
+        } else {
+            emailArea.style.backgroundColor = "red";
+            nameArea.style.backgroundColor = "red";
+            textArea.style.backgroundColor = "red";
+        }
+    })    
+    
+
+    //For the **Email** field, to check if it contains the `@` character and value length has to be greater than zero
+  
+
+    //For the **Your name** field, to check if value length has to be greater than zero
+    
+//     if(nameArea.value.trim().length > 0) {
+//         nameArea.style.backgroundColor = "transparent";
+//         
+//     } else {
+//         nameArea.style.backgroundColor = "red";
+//     };
+
+//     //For the **Your name** field, to check if value length has to be greater than zero
+    
+//     if(textArea.value.trim().length > 0) {
+//         textArea.style.backgroundColor = "transparent";
+//         
+//     } else {
+//         textArea.style.backgroundColor = "red";
+//     };
+  
+// };
+// function allFilled() {
+//     if(isCheckUserInputValid) {
+//         alert("Thank you for filling out the form");
+// }
+
+
+
+// let form = document.querySelector("form");
+// form.addEventListener("submit", checkUserInput)
+// let emailArea = document.querySelector("#exampleInputEmail1");
+// let nameArea = document.querySelector("#example-text-input");
+// let textArea = document.querySelector("#exampleTextarea");
+// let inputs = document.querySelectorAll("input");
+// let inputsArray = Array.from(inputs); 
+
+// emailArea.addEventListener("change", changeBackground);
+// nameArea.addEventListener("change", changeBackground);
+// textArea.addEventListener("change", changeBackground);
+
+// function changeBackground(event) {
+//     event.target.classList.remove("redBackground");
+//     event.target.classList.add("whiteBackground");
+// }
+// function checkUserInputs(event) {
+//   event.preventDefault();
+//   let isEmailAreaValid = emailArea.value.trim().includes("@");
+//   let isNameAreaValid = nameArea.value.trim().length > 0;
+//   let isTextAreaValid = textArea.value.trim().length > 0;
+//   if(isEmailAreaValid && isNameAreaValid && isTextAreaValid) {
+//       emailArea.value = "";
+//       nameArea.value = "";
+//       textArea.value = "";
+//       emailArea.style.backgroundColor = "white";
+//       nameArea.style.backgroundColor = "white";
+//       textArea.style.backgroundColor = "white";
+//       alert("Thank you for filling out the form");
+//   } else {
+//       if(!isEmailAreaValid) {
+//           emailArea.classList.remove("whiteBackground");
+//           emailArea.classList.add("redBackground");
+//       }
+//       if(!isNameAreaValid) {
+//           nameArea.classList.remove("whiteBackground");
+//           nameArea.classList.add("redBackground");
+//       }
+//       if(!isTextAreaValid) {
+//           textArea.classList.remove("whiteBackground");
+//           nameArea.classList.add("redBackground");
+//       }
+
+//   }
+// }
+
+
